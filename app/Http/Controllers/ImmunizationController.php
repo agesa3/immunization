357989c64ef53a7d9d3af02e92731f3e7bb085d1
agesa3 <collinsagesa2000@gmail.com>
2022-02-23 17:25:38 +0300

    immunization model and controller created

---
+++ app/Http/Controllers/ImmunizationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Immunization;
+use Illuminate\Http\Request;
+
+class ImmunizationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Immunization  $immunization
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Immunization $immunization)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Immunization  $immunization
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Immunization $immunization)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Immunization  $immunization
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Immunization $immunization)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Immunization  $immunization
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Immunization $immunization)
+    {
+        //
+    }
+}
